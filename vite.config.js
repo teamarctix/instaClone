7809912692f5725fe8231f5c0bcfe03be0e0ba68
ITZ ARCTIX <144344981+teamarctix@users.noreply.github.com>
2025-06-01 01:44:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: "/instaClone/",   // <--- add this, replace instaClone with your repo name
   plugins: [react()],
   resolve: {
     alias: {
